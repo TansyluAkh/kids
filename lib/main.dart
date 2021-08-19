d3fc4f0febdccde13dd65f2fd14ad5f025a6b523
SightRo <tagir00123@gmail.com>
2021-08-19 21:17:04 +0300

    Handle Firebase properly

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:bebkeler/models/user.dart';
 import 'package:bebkeler/screens/splash_screen.dart';
 import 'package:bebkeler/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
