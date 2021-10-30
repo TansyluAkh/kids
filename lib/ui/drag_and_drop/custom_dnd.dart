7157473b620473971f8416f56528d93340e0bff3
SightRo <tagir00123@gmail.com>
2021-10-31 02:18:14 +0300

    The most beautiful and simple af solution - Use Transform.translate instead of overlay hassle - Still need check how to add drop functionality

--- lib/ui/drag_and_drop/custom_dnd.dart
+++ lib/ui/drag_and_drop/custom_dnd.dart
@@ -21,43 +21,25 @@ class OffsetTween extends Tween<Offset?> {
 }
 
 class _DraggableCardState extends State<DraggableCard> with SingleTickerProviderStateMixin {
-  OverlayEntry? _overlay;
   late AnimationController _controller;
   OffsetTween? _offsetTween;
-  Offset? _currentPosition;
-  Offset? _startPosition;
+  Offset _currentPosition = Offset.zero;
+  Offset _startPosition = Offset.zero;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(vsync: this, duration: Duration(seconds: 1));
     _controller.addListener(() {
-      _currentPosition = _offsetTween!.evaluate(_controller);
-      _overlay!.markNeedsBuild();
-    });
-
-    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
-      _updateStartPosition();
-      _addOverlay();
+      _currentPosition = _offsetTween!.evaluate(_controller)!;
+      setState(() {});
     });
   }
 
   @override
   void dispose() {
     _controller.dispose();
-    _overlay!.remove();
-    _overlay = null;
     super.dispose();
-  }
-
-  void _updateStartPosition() {
-    if (_startPosition == null) {
-      final box = context.findRenderObject() as RenderBox;
-      final offset = box.localToGlobal(Offset.zero);
-
-      _startPosition = offset;
-      _currentPosition = _startPosition;
-    }
   }
 
   void _runAnimation() {
@@ -66,36 +48,23 @@ class _DraggableCardState extends State<DraggableCard> with SingleTickerProvider
     _controller.forward(from: 0);
   }
 
-  void _addOverlay() {
-    if (_overlay != null) return;
-    _overlay = OverlayEntry(builder: (context) {
-      return Positioned(
-        top: _currentPosition!.dy,
-        left: _currentPosition!.dx,
-        child: GestureDetector(
-          onPanDown: (details) {
-            if (_controller.isAnimating) {
-              _currentPosition = _offsetTween!.evaluate(_controller);
-              _controller.stop();
-            }
-          },
-          onPanUpdate: (details) {
-            _currentPosition = _currentPosition! + details.delta;
-            _overlay!.markNeedsBuild();
-          },
-          onPanEnd: (details) {
-            _runAnimation();
-          },
-          child: widget.child,
-        ),
-      );
-    });
-
-    Overlay.of(context)!.insert(_overlay!);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onPanDown: (details) {
+        if (_controller.isAnimating) {
+          _currentPosition = _offsetTween!.evaluate(_controller)!;
+          _controller.stop();
+        }
+      },
+      onPanUpdate: (details) {
+        _currentPosition = _currentPosition + details.delta;
+        setState(() {});
+      },
+      onPanEnd: (details) {
+        _runAnimation();
+      },
+      child: Transform.translate(offset: _currentPosition, child: widget.child),
+    );
   }
 }
